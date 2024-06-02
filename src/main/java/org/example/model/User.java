0e1a23759f5bb6f8dc17497ae0608310eed6ceee
NurmukhametovAlexey <srgalex@list.ru>
2024-06-02 11:33:53 +0300

    discuss serialization

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,3 +1,11 @@
 package org.example.model;
 
-public record User(Long id, String login, String password) {}
+import org.apache.commons.lang3.StringUtils;
+
+public record User(Long id, String login, String password) {
+    // 1230000000
+    // 0000000123
+    String getIdZeroPadded() {
+        return StringUtils.leftPad(id.toString(), 10, '0');
+    }
+}
--- src/main/java/org/example/requestProcessor/user/UsersRequestProcessor.java
+++ src/main/java/org/example/requestProcessor/user/UsersRequestProcessor.java
@@ -1,6 +1,12 @@
 package org.example.requestProcessor.user;
 
+import java.lang.reflect.Field;
+import java.text.ParseException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import jakarta.ws.rs.core.Response;
 import org.example.exception.MethodNotAllowedException;
@@ -36,13 +42,82 @@ public class UsersRequestProcessor implements ReqProcessor {
         if (!userList.isEmpty()) {
             responseProcessor.returnBody(httpRequest, Response.Status.OK, userList);
         } else {
+            responseProcessor.returnNoBody(httpRequest, Response.Status.NO_CONTENT);
             throw new NoContentException();
         }
     }
 
-    private void postRequest(HttpRequest httpRequest) {
+    private void postRequest(HttpRequest httpRequest) throws IllegalAccessException {
+        MyDeserializer des = null;
+        User userToCreate = des.parse(httpRequest.getBody(), User.class);
+
+        User myUser = null;
+//        {
+//            "login": "abc",
+//                "email": "abd@mail.ru",
+//                "roles": [
+//            {"type": "ADMIN", "createdAt": "abc"},
+//            {"type": "USER", "createdAt": "2020-01-01"}
+//          ]
+//        }
+        MySerializer ser = null;
+//        ser.preferredType = FIELD/METHOD
+        ser.writeAsString(myUser);
+
+
+
+
+
+        /// cache - последняя очередь, после того как все заработает
+        // без кеша - 10 мс
+        // с кешем - 1 мс
+
+        StringBuilder sb = new StringBuilder();
+        Class<? extends User> userClass = myUser.getClass();
+        String rootTag = userClass.getSimpleName();
+
+        sb.append("<").append(rootTag).append(">");
+
+
+        Field[] fields = userClass.getFields();
+//        userClass.getMethods()
+                // getName -> "name": "vasya"
+                // isActive -> "active": true
+        Map<String, FieldGetter> fieldNameToGetter = new HashMap<>();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            fieldNameToGetter.put(field.getName(), field::get);
+        }
+
+        // ()()[]()
+        // ((([])[][]))
+        // (([)])
+        // { "dssdf": "fds", "nestedObjects": [{...}, {...}] }
+        // String -> "abc"
+        // Number -> 123
+        // Boolean -> true/false
+
+        for (Map.Entry<String, FieldGetter> entry : fieldNameToGetter.entrySet()) {
+            String name = entry.getKey();
+            FieldGetter getter = entry.getValue();
+            Object value = getter.getValue(myUser);
+            sb.append(name);
+            // value - примитив - пишем
+            sb.append(value);
+            // value - pojo -
+            sb.append(ser.writeAsString(value));
+        }
+
+
+        // мы можем 1 раз посчитать
+        Map<Class<?>, <rootTag,fieldNameToGetter> > classToHowToSerialize = new HashMap<>();
+
+
+
+
 //        if (httpRequest.hadBody()) {
-//            UserForm userForm = userFormParsing(httpRequest.getBody());
+//            MyDeserializer des = null;
+//            User userForm = des.parse(httpRequest.getBody(), User.class);
 //            usersService.addUser(userForm);
 //        } else {
             // There is no body to create User.
@@ -50,6 +125,9 @@ public class UsersRequestProcessor implements ReqProcessor {
 //        }
     }
 
+    interface FieldGetter {
+        Object getValue(Object owner) throws IllegalAccessException;
+    }
 
 //    GET domain.com/my-api/users -> список юзеров
 //    POST domain.com/my-api/users + body -> создать нового
